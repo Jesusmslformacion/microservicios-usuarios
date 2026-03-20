1b05e74df2759628fbf491f183c7a5bfb8d736b7
Jesusmslformacion <chechuelcrack@gmail.com>
2026-03-20 21:50:51 +0100

    Creacion proyecto de microservicios, package models y el Entity Alumno

---
+++ src/main/java/com/jesus/microservicios/app/usuarios/microservicios_usuarios/models/entity/Alumno.java
@@ -0,0 +1,10 @@
+package com.jesus.microservicios.app.usuarios.microservicios_usuarios.models.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="usuarios")
+public class Alumno {
+
+}
