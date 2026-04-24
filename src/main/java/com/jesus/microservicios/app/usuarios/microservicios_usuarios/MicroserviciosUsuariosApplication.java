ff4d69f6f1c66e5907cb2285bc8412625f78f6d8
AppMod Upgrade Agent <upgrade@appmod.local>
2026-04-24 13:05:07 +0200

    Agregar anotación @EntityScan para incluir entidades de commons-alumnos en MicroserviciosUsuariosApplication

--- src/main/java/com/jesus/microservicios/app/usuarios/microservicios_usuarios/MicroserviciosUsuariosApplication.java
+++ src/main/java/com/jesus/microservicios/app/usuarios/microservicios_usuarios/MicroserviciosUsuariosApplication.java
@@ -2,9 +2,11 @@ package com.jesus.microservicios.app.usuarios.microservicios_usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.jesus.microservicios.commons.alumnos.commons_alumnos.models.entity"})
 public class MicroserviciosUsuariosApplication {
 
 	public static void main(String[] args) {
