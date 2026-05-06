8c25b0068026bff0c1d511aba583d07ef2be9514
AppMod Upgrade Agent <upgrade@appmod.local>
2026-05-06 21:06:36 +0200

    Agregar funcionalidad de filtrado de alumnos por nombre o apellido en AlumnoController, AlumnoRepository y AlumnoService

--- src/main/java/com/jesus/microservicios/app/usuarios/microservicios_usuarios/services/AlumnoService.java
+++ src/main/java/com/jesus/microservicios/app/usuarios/microservicios_usuarios/services/AlumnoService.java
@@ -1,11 +1,13 @@
 package com.jesus.microservicios.app.usuarios.microservicios_usuarios.services;
 
 
+import java.util.List;
+
 import com.jesus.microservicios.commons.alumnos.commons_alumnos.models.entity.Alumno;
 import com.jesus.microservicios.commons.commons_microservicios.services.CommonService;
 
 public interface AlumnoService extends CommonService<Alumno> {
 
-
+    public List<Alumno> findByNombreOrApellido(String term);
     
 }
--- src/main/java/com/jesus/microservicios/app/usuarios/microservicios_usuarios/services/AlumnoServiceImpl.java
+++ src/main/java/com/jesus/microservicios/app/usuarios/microservicios_usuarios/services/AlumnoServiceImpl.java
@@ -1,14 +1,24 @@
 package com.jesus.microservicios.app.usuarios.microservicios_usuarios.services;
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jesus.microservicios.app.usuarios.microservicios_usuarios.models.repository.AlumnoRepository;
 import com.jesus.microservicios.commons.alumnos.commons_alumnos.models.entity.Alumno;
 import com.jesus.microservicios.commons.commons_microservicios.services.CommonServiceImpl;
 
+
 @Service
 public class AlumnoServiceImpl extends CommonServiceImpl <Alumno, AlumnoRepository> implements AlumnoService{
 
+    @Override
+    @Transactional(readOnly = true)
+    public List<Alumno> findByNombreOrApellido(String term) {
+        return repository.findByNombreOrApellido(term);
+    }
 
 }
+    
